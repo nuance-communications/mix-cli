0ab6108548036cc2f7a4f982aa8e9fdbea363d39
Oliver Daniel <oliver.daniel@nuance.com>
2022-05-31 11:47:01 -0400

    feat: scaffold channels test data
    Signed-off-by: Oliver Daniel <oliver.daniel@nuance.com>

---
+++ test/commands/channels/channels-test-data.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+    rename: {
+        flags: {
+            project: '123',
+            channel: 'very-long-uuid',
+            'new-name': 'renamed channel'
+        },
+        response: {
+            data: {
+                displayName: 'renamed channel'
+            }
+        }
+    }
+}
