53ba98fedfc0195ca70c41219c9a5b534fef3f26
Oliver Daniel <oliver.daniel@nuance.com>
2022-06-06 14:12:16 -0400

    feat: add api types for create channels

--- src/mix/api/channels-types.ts
+++ src/mix/api/channels-types.ts
@@ -6,19 +6,51 @@
  * the LICENSE file in the root directory of this source tree.
  */
 
+import {channelColors} from './utils/channel-colors'
+
 /** @hidden */
 export type ChannelsPathParams = {
-    /** ID of the relevant project. */
-    projectId: string;
+  /** ID of the relevant project. */
+  projectId: string;
 
-    /** ID of the relevant channel. */
-    channelId: string;
+  /** ID of the relevant channel. */
+  channelId: string;
 }
 
 /** @hidden */
 export type ChannelsRenameBodyParams = {
-    /** Updated channel display name */
-    displayName: string;
+  /** Updated channel display name */
+  displayName: string;
 }
 
+/** @hidden */
+export type ChannelsCreatePathParams = {
+  /** ID of the relevant project. */
+  projectId: string;
+}
+
+// REVIEW: this should almost certainly be put in its own file
+type ChannelModality =
+    'MODE_UNSPECIFIED' |
+    'RICH_TEXT' |
+    'TTS' |
+    'INTERACTIVITY' |
+    'AUDIO_SCRIPT' |
+    'DTMF'
+
+type ChannelColor = typeof channelColors[number];
+
+/** @hidden */
+export type ChannelsCreateBodyParams = {
+  /** Channel display name. */
+  displayName: string;
+
+  /** Modes available for the channel. */
+  modes: ChannelModality[];
+
+  /** Channel color. */
+  color: ChannelColor;
+}
+
+export type ChannelsCreateParams = {projectId: string} & ChannelsCreateBodyParams
 export type ChannelsRenameParams = ChannelsPathParams & ChannelsRenameBodyParams
