fa1c929bc2c09d35351a8b9fa4240770acf2be02
Oliver Daniel <oliver.daniel@nuance.com>
2022-07-05 14:11:43 -0400

    feat: create new feature branch on github instance

---
+++ src/commands/channels/rename.ts
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
+import chalk from 'chalk'
+import {flags} from '@oclif/command'
+import makeDebug from 'debug'
+
+import * as MixFlags from '../../utils/flags'
+import * as ChannelsAPI from '../../mix/api/channels'
+import {DomainOption} from '../../utils/validations'
+import {MixClient, MixResponse} from '../../mix/types'
+import MixCommand from '../../utils/base/mix-command'
+import {Output} from '@oclif/parser/lib/flags'
+import {ChannelsRenameParams} from '../../mix/api/channels-types'
+
+const debug = makeDebug('mix:commands:channels:rename')
+
+export default class ChannelsRename extends MixCommand {
+  static description = `rename a channel in Mix project
+    
+    Use this command to change the name of a channel in a project.`
+
+  static examples = [
+    'mix channels:rename -P 1922 \\',
+    '  --channel bc40667c-e0f6-11ec-9d64-0242ac120003 \\',
+    '  --new-name "voice channel"',
+  ]
+
+  static flags = {
+    project: MixFlags.projectWithDefaultFlag,
+    channel: {
+      ...MixFlags.channelMultipleFlag, // REVIEW: not too happy with this
+      multiple: false,
+    },
+    'new-name': flags.string({
+      required: true,
+      description: 'New channel name',
+    }),
+    ...MixFlags.machineOutputFlags,
+  }
+
+  get domainOptions(): DomainOption[] {
+    debug('get domainOptions()')
+    return ['project']
+  }
+
+  async buildRequestParameters(options: Partial<Output>): Promise<ChannelsRenameParams> {
+    debug('buildRequestParameters()')
+    const {'new-name': displayName, project: projectId, channel: channelId} = options
+
+    return {projectId, channelId, displayName}
+  }
+
+  doRequest(client: MixClient, params: ChannelsRenameParams): Promise<MixResponse> {
+    debug('doRequest()')
+    return ChannelsAPI.renameChannel(client, params)
+  }
+
+  outputHumanReadable(transformedData: any) {
+    debug('outputHumanReadable()')
+    const {displayName} = transformedData
+    this.log(`Channel renamed to ${chalk.cyan(displayName)}.`)
+  }
+}
---
+++ test/commands/channels/rename.test.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
+import { expect, test } from '@oclif/test'
+
+const td = require('./channels-test-data')
+// for endpoint construction
+const { project: projectId, channel: channelId } = td.rename.flags
+
+const testEnvData = require('../../test-data')
+const serverURL = `https://${testEnvData.server}`
+const endpoint = `/v4/projects/${projectId}/channels/${channelId}/.rename`
+
+
+describe('channels:rename', () => {
+  const { project, channel, 'new-name': newName } = td.rename.flags
+
+  test
+    .env(testEnvData.env)
+    .nock(serverURL, (api) =>
+      api
+        .put(endpoint, {
+          displayName: newName
+        })
+        .reply(200, td.rename.response.data)
+    )
+    .stdout()
+    .command(['channels:rename',
+      '--project=' + project,
+      '--channel=' + channel,
+      '--new-name=' + newName,
+      '--json'])
+    .it('receives JSON confirmation of renamed channel', ctx => {
+      const response = JSON.parse(ctx.stdout)
+      expect(response).to.deep.equal({ displayName: newName })
+    })
+})
