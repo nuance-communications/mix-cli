20a87f2086f643527ee8760defa3f8dce7e75aa8
Francois Groleau <francois.groleau@nuance.com>
2022-07-11 16:51:21 -0400

    feat: add common files for intents commands

---
+++ src/mix/api/intents-types.ts
@@ -0,0 +1,10 @@
+/* eslint-disable unicorn/no-empty-file */
+/*
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
+// import {Expand} from './shared-types'
