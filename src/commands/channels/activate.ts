bbd9027df0f6d1f2b3b29403d41de7084e781121
Oliver Daniel <oliver.daniel@nuance.com>
2022-08-08 15:17:08 -0400

    fix: re-align copyright notice

--- src/commands/channels/activate.ts
+++ src/commands/channels/activate.ts
@@ -1,10 +1,10 @@
 /*
-  * Copyright 2022, Nuance, Inc. and its contributors.
-  * All rights reserved.
-  *
-  * This source code is licensed under the Apache-2.0 license found in
-  * the LICENSE file in the root directory of this source tree.
-  */
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
 
 import {flags} from '@oclif/command'
 import chalk from 'chalk'
