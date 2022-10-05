1adc725f9ca7bf28f473e4ad74708638194a8c0e
kruti49 <krutipatel68@gmail.com>
2022-10-05 10:28:42 -0400

    fix: bots:list output

--- src/commands/bots/list.ts
+++ src/commands/bots/list.ts
@@ -108,10 +108,7 @@ A number of flags can be used to constrain the returned results.`
 
     if (options.full) {
       this.warn(`Full bot configuration objects are complex, so some data is not shown.
-If you want to see all bot configs, either:
-retry the command in JSON or YAML mode, or
-use bots:get to get full details for a single bot.
-`)
+      If you want to see the complete data then retry the command with the output format set to JSON or YAML.`)
     }
 
     super.outputCLITable(transformedData, columns)
