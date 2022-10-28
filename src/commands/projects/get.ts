2af799b9cd044f89509eaec49559dd5115c9e594
kruti49 <krutipatel68@gmail.com>
2022-10-28 18:00:50 -0400

    fix: projects-get output

--- src/commands/projects/get.ts
+++ src/commands/projects/get.ts
@@ -108,6 +108,12 @@ information at a time. The chosen section is specifed using the --table flag.`
 
   outputHumanReadable(transformedData: any) {
     debug('outputHumanReadable()')
+    if (transformedData === undefined) {
+      this.log('No project found.')
+
+      return
+    }
+
     // This outputs the project info using key-value pairs
     // and the channels and data packs using tables.
     this.outputProjectInfo(transformedData)
