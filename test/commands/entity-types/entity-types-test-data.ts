699515ef91fc5561aec89843d2a9f2a53badac36
Krutika Patel <83274011+kruti49@users.noreply.github.com>
2022-12-01 15:15:44 -0500

    test: add entity-types-list unit tests (#119)
    test: add entity-types-list unit tests

---
+++ test/commands/entity-types/entity-types-test-data.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2022, Nuance, Inc. and its contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+
+export default {
+  entityTypesListResponse: {
+    entityTypes: [
+      {
+        name: 'BASE',
+        description: 'Predefined Entity',
+        compatibleDataTypes: [
+          'SET'
+        ]
+      },
+      {
+        name: 'FREEFORM',
+        description: 'Freefrom text',
+        compatibleDataTypes: [
+          'SET',
+          'NUMBER'
+        ]
+      }
+    ]
+  },
+  noEntityTypesResponse: {
+    entityTypes: []
+  }
+}
