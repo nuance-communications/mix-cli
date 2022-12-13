5de39e96a20f5d31e40b93fd7182894392013992
Francois Groleau <francois.groleau@nuance.com>
2022-12-13 18:06:02 -0500

    fix: correct description of views

--- src/mix/api/applications-types.ts
+++ src/mix/api/applications-types.ts
@@ -9,11 +9,13 @@
 import {Expand} from './shared-types'
 
 /**
- * Application credentials view to return.
- * - ACV_VIEW_UNSPECIFIED: Returns credentials details without including clients
- * - ACV_FULL: Returns all credentials details, including list of clients
+ * Application view to return.
+ * - AV_VIEW_UNSPECIFIED: Returns application details without including application configurations
+ * - AV_FULL: Returns all application details, including the list of application configurations
+ * - AV_FULL_AVAILABLE_CONFIGS: Returns all application details, omitting configs that are overridden
+ * - AV_FULL_LIVE_CONFIGS: Returns all application configs that are deployed
  *
- * @defaultValue: ACV_VIEW_UNSPECIFIED
+ * @defaultValue: AC_VIEW_UNSPECIFIED
  */
 export type ApplicationsView = 'AV_VIEW_UNSPECIFIED' | 'AV_FULL' | 'AV_FULL_AVAILABLE_CONFIGS' | 'AV_FULL_LIVE_CONFIGS'
 
@@ -41,8 +43,8 @@ export type ApplicationsListSearchParams = {
    * - AV_FULL_AVAILABLE_CONFIGS: Returns all application details, omitting configs that are overridden
    * - AV_FULL_LIVE_CONFIGS: Returns all application configs that are deployed
    *
-   * @defaultValue: ACV_VIEW_UNSPECIFIED
-  */
+   * @defaultValue: AC_VIEW_UNSPECIFIED
+   */
   view?: ApplicationsView
 }
 
