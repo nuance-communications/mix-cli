5458eda7cd87fd0954d0d8c131f9a27058eedb9e
kruti49 <krutipatel68@gmail.com>
2022-12-16 17:39:30 -0500

    test: applications-get output

--- test/commands/applications/get.test.ts
+++ test/commands/applications/get.test.ts
@@ -37,10 +37,10 @@ describe('applications:get command', () => {
       const lines = ctx.stdout.split('\n').map(ln => ln.trim())
       const headers = lines[4].split(/\s+/)
       const firstRow = lines[6].split(/\s+/)
-      ////test if the resulted applicationId is same as provided applicationId
+      //test if the resulted applicationId is same as provided applicationId
       expect(lines[0]).to.deep.equal(`ApplicationId: ${applicationId}`)
       expect(lines[1]).to.deep.equal('Name: Sample App')
-      expect(lines[1]).should.not.equal(`Name: Sample App ${applicationId}`)
+      expect(lines[1]).to.not.contain(`${applicationId}`)
       expect(lines[2]).to.deep.equal('CreateTime: later')
       expect(headers).to.deep.equal(['ConfigId', 'DeploymentFlowId', 'ProjectId', 'ProjectName', 'CreateTime'])
       expect(firstRow).to.deep.equal('58 6 61 Test Project later'.split(/\s+/))
